64c08c2102ccd0c14b7bfced16f21fd8ed1c4a78
adrian19 <adrian19lb@gmail.com>
2019-04-02 20:05:07 +0200

    Add doc for FlagGenerator class

--- flagappender/flag_appender.py
+++ flagappender/flag_appender.py
@@ -2,19 +2,39 @@ from abc import ABC, abstractmethod
 from filepathsgenerator import filepaths_generator
 
 class FlagGenerator(ABC):
-    """Docstring for FlagGenerator. """
+    """
+    Abstract base class for flags generators implementation. All classes, which inherit from
+    this one must override generate_flags() method
+        
+    Args:
+        filenames_holder(FilenamesGenerator): Given container with filepaths mapper. This arg 
+        will be used for compute flags 
+
+    Attributes:
+        filenames_holder(FilenamesGenerator): Internal variable to store FilenamesGenerator 
+    """
 
     def __init__(self, filenames_holder):
-        """TODO: to be defined1. """
         self.filenames_holder = filenames_holder
     
     def generate(self, filename):
+        """ Generate flags by given filename and dependent filenames
+            
+            Args: 
+                filename(str): Name of file
+            Raises:
+                NoFileError: File doesn't exist
+            Returns:
+                created_flag
+        """
         dependent_file_list = self.filenames_holder.generate(filename)
         
         return self.generate_flags(dependent_file_list)
 
     @abstractmethod
     def generate_flags(self, dependent_file_list):
+        """ Abstract method for implementation of way generating flags
+        """
         return
 
 class PrependedFlagGenerator(FlagGenerator):
