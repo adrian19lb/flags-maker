a5fe83219a1e3859e440243e3976634a44a50f7f
adrian19 <adrian19lb@gmail.com>
2019-04-02 18:48:27 +0200

    Create setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    # Metadata
+    name = 'flagsmaker',
+    version = '1.0',
+    license = 'MIT',
+    description = 'Simple Python flag generator for C-family language',
+    url = 'https://github.com/adrian19lb/flags-maker',
+    author = 'Adrian Demianiuk',
+    author_email = 'adrian19lb@gmail.com',
+    packages = ['filepathsgenerator', 'flagappender']
+)
